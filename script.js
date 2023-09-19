bd110e77c6c730c48d6796c563efc312f4ddf8d7
Rohan0004 <110601451+Rohan0004@users.noreply.github.com>
2023-09-19 13:41:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+let btn=document.getElementsByTagName("input")[0];
+btn.addEventListener("click",()=>{
+    let val=document.getElementById("colorSelect");
+    val.remove(val.selectedIndex);
+});
